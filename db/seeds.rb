ddcc19f29025b56ee855e7449b3641476783d4d1
Mathias <mathias.richard.schneider@gmail.com>
2021-06-08 00:45:20 -0500

    adds places api backend

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+  place = Place.create(name: "The Tracks", address: "108 W Main St")
+  place = Place.create(name: "Chick-fil-A", address: "4812 Northwest Hwy")
